cf3d6dbf2460815068bb732583950b8a04c3cb45
iloveangpao <lim.yan@bridgesforenterprise.com>
2024-09-02 04:13:40 +0800

    dummy login

---
+++ my-angular-dashboard/src/app/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  constructor(private router: Router) {}
+
+  login() {
+    // Simulate a successful login and store a mock JWT token
+    localStorage.setItem('token', 'mock-jwt-token');
+    this.router.navigate(['/dashboard']); // Navigate to the dashboard
+  }
+}
