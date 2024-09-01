c691542e3c6ec7ef30b6a753ad47f6f5d1322abb
iloveangpao <lim.yan@bridgesforenterprise.com>
2024-09-02 04:11:51 +0800

    Basic UI for dashboard content

--- my-angular-dashboard/src/app/app.component.ts
+++ my-angular-dashboard/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, 
+    HeaderComponent,
+    SidebarComponent,
+    ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ my-angular-dashboard/src/app/content/content.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.scss'],
+  standalone: true,
+})
+export class ContentComponent {
+  @Input() data: any[] = [];
+}
+
+
