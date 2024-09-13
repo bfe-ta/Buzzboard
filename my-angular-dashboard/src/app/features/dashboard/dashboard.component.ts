bc798a0d8b5e9670f25882ab0b8069f75c8038df
iloveangpao <lim.yan@bridgesforenterprise.com>
2024-09-13 14:40:11 +0800

    reorg

--- my-angular-dashboard/src/app/features/dashboard/dashboard.component.ts
+++ my-angular-dashboard/src/app/features/dashboard/dashboard.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { HeaderComponent } from '../header/header.component';
-import { SidebarComponent } from '../sidebar/sidebar.component';
-import { ContentComponent } from '../content/content.component';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
+import { ContentComponent } from '../../shared/content/content.component';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
