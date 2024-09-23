cd3baf652d87586f0bf57d7ab3ee6131bffd8af9
iloveangpao <lim.yan@bridgesforenterprise.com>
2024-09-24 02:48:51 +0800

    Revert "content generative(supp to be shared)"
    This reverts commit 0a612d708e363596ab8d5d8da8031f2411fd82ed.

--- my-angular-dashboard/src/app/shared/components/content/content.component.ts
+++ my-angular-dashboard/src/app/shared/components/content/content.component.ts
@@ -1,19 +1,13 @@
-import { Component, Input, TemplateRef } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-content',
-  standalone: true,
-  imports: [CommonModule],  // Standalone component
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss']
+  styleUrls: ['./content.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
 })
 export class ContentComponent {
-  @Input() data: any[] = [];  // Dynamic data input
-  @Input() template!: TemplateRef<any>;  // Template reference for dynamic content rendering
-
-  trackByFn(index: number, item: any): number {
-    return index;  // Track by index for better performance
-  }
+  @Input() data: any[] = [];
 }
-
