8c620f932cf4a1e5821b200dc024574a4d52f7dc
iloveangpao <lim.yan@bridgesforenterprise.com>
2024-09-24 03:21:28 +0800

    generate title

--- my-angular-dashboard/src/app/features/components/dashboard/dashboard.component.ts
+++ my-angular-dashboard/src/app/features/components/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from '../../../shared/components/header/header.compon
 import { SidebarComponent } from '../../../shared/components/sidebar/sidebar.component';
 import { ContentComponent } from '../../../shared/components/content/content.component';
 import { DataManagementService } from '../../../core/data/data-management.service';
+import { TokenService } from '../../../core/tokens/token.service';
 import { CommonModule } from '@angular/common';
 import { DatePipe } from '@angular/common';  // Import DatePipe
 @Component({
@@ -20,9 +21,10 @@ export class DashboardComponent implements OnInit {
   mainTitle: string = '';  // Dynamic main title
   subTitle: string = 'Private Buzzloop Leaderboard';  // Dynamic subtitle
 
-  constructor(private dataManagementService: DataManagementService, private datePipe: DatePipe) {}
+  constructor(private dataManagementService: DataManagementService, private datePipe: DatePipe, private tokenService: TokenService) {}
 
   ngOnInit(): void {
+    this.setMainTitle();
     this.loadData();
   }
 
@@ -42,5 +44,15 @@ export class DashboardComponent implements OnInit {
     const now = new Date();
     // Format date using Angular's DatePipe (e.g., '28/08/2024 14:30:15')
     this.updatedOn = this.datePipe.transform(now, 'dd/MM/yyyy HH:mm:ss') || '';
+  }
+
+  setMainTitle(): void {
+    const user = this.tokenService.getUser();  // Retrieve user data from session storage
+
+    if (user && user.company) {
+      this.mainTitle = `${user.company}'s`;  // Set the main title to the company name
+    } else {
+      this.mainTitle = "Company's";  // Fallback in case user or company is undefined
+    }
   }
 }
