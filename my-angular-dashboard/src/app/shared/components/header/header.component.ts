fee916d60995e39e392bc195ade42ec94dec4ede
iloveangpao <lim.yan@bridgesforenterprise.com>
2024-11-05 22:18:32 +0800

    logout

--- my-angular-dashboard/src/app/shared/components/header/header.component.ts
+++ my-angular-dashboard/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { UserManagementService } from '../../../core/user-management/user-management.service';
 
 @Component({
   selector: 'app-header',
@@ -10,5 +11,11 @@ export class HeaderComponent {
   @Input() logoUrl: string = '';  // Input for the logo image URL
   @Input() mainTitle: string = '';  // Input for the main title (e.g., "PWC's")
   @Input() subTitle: string = '';  // Input for the subtitle (e.g., "Private Buzzloop Leaderboard")
+
+  constructor(private userManagementService: UserManagementService) {}
+
+  logout() {
+    this.userManagementService.logout();
+  }
 }
 
