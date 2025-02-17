ef6a05d71dfc6af8d51664476478a253c6421b75
Gwenyth <arqhlls88@gmail.com>
2025-02-17 17:13:00 +0800

    80

--- my-angular-dashboard/src/app/services/auth.service.ts
+++ my-angular-dashboard/src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
 
-  private apiUrl = 'http://localhost:8080/api/auth/signin ';  
+  private apiUrl = 'http://localhost:80/api/auth/signin ';  
   constructor(private http: HttpClient) {}
 
   login(credentials: { email: string; password: string }): Observable<any> {
